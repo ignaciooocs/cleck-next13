f15136729c3f3f43ac9bc9f86c57539d9005eb60
ignacioocode <naxomc25@gmail.com>
2023-07-31 02:00:31 -0400

    New corrected environment variables error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
-if (typeof process.env.ADD !== 'string') throw new Error('La variable de entorno no se configuró correctamente')
-if (typeof process.env.GET !== 'string') throw new Error('La variable de entorno no se configuró correctamente')
-
 export default authMiddleware({
-
-  ignoredRoutes: [process.env.ADD, process.env.GET],
+  ignoredRoutes: ['/api/add ', '/api/notes/:id'],
   publicRoutes: ['/']
 })
 
